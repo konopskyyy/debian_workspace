c629f7254d166590843919025ea22ed102f0d6c7
konopskyyy <konopskyyy@gmail.com>
2026-04-02 07:24:00 +0200

    add: install basicx ai tools

--- installer.sh
+++ installer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 bash component/install_package_managers.sh
-#bash component/install_docker.sh
-#bash component/install_git_prompt.sh
+bash component/install_docker.sh
+bash component/install_git_prompt.sh
 bash component/install_node.sh
+bash component/install_ai_tools.sh
